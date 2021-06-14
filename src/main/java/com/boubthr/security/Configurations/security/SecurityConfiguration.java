020f5ca5b079f3cb409cc7a046e35a04a0e7277c
Boub <tahiri.boub@gmail.com>
2021-06-14 11:44:15 +0100

    add authority and permission based auth..

--- src/main/java/com/boubthr/security/Configurations/security/SecurityConfiguration.java
+++ src/main/java/com/boubthr/security/Configurations/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.boubthr.security.Configurations.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -33,6 +34,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/","index","/css/*","/js/*")
                 .permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(UserPermission.COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(UserPermission.COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(UserPermission.COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET,"/management/api/vi/students").hasAnyRole(ADMIN.name(),ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,17 +50,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         UserDetails aUser =  User.builder()
                 .username("boub")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+                //.roles(STUDENT.name()) // => ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthority())
                 .build();
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("admin@123"))
-                .roles(ADMIN.name())
+                //.roles(ADMIN.name()) // => ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthority())
                 .build();
         UserDetails trainee = User.builder()
                 .username("trainee")
                 .password(passwordEncoder.encode("trainee@123"))
-                .roles(ADMIN_TRAINEE.name())
+                //.roles(ADMIN_TRAINEE.name()) // => ROLE_ADMIN_TRAINEE
+                .authorities(ADMIN_TRAINEE.getGrantedAuthority())
                 .build();
         return new InMemoryUserDetailsManager(aUser, admin, trainee);
     }
--- src/main/java/com/boubthr/security/Configurations/security/UserRole.java
+++ src/main/java/com/boubthr/security/Configurations/security/UserRole.java
@@ -1,8 +1,11 @@
 package com.boubthr.security.Configurations.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.boubthr.security.Configurations.security.UserPermission.*;
 
@@ -19,5 +22,13 @@ public enum UserRole {
 
     public Set<UserPermission> getPermissions() {
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthority() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return permissions ;
     }
 }
